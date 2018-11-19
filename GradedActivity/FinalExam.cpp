ccffb70dc67cbe5277967a2dffc682c91e0ab1c9
Mike Lisak <lisakmike@gmail.com>
2018-11-19 02:59:53 -0600

    Defined adjustScore in FinalExam.cpp

--- GradedActivity/FinalExam.cpp
+++ GradedActivity/FinalExam.cpp
@@ -12,4 +12,16 @@ void FinalExam::set(int questions, int missed)
 	numericScore = 100.0 - (missed * pointsEach);
 
 	setScore(numericScore);
+
+	adjustScore();
+}
+
+void FinalExam::adjustScore()
+{
+	double fraction = score - static_cast<int>(score);
+
+	if (fraction >= 0.5)
+	{
+		score += (1.0 - fraction);
+	}
 }
