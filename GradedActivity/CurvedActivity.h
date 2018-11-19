f39873cb191b0fcc93e61792da78585032aeb2d4
Mike Lisak <lisakmike@gmail.com>
2018-11-19 16:19:47 -0600

    Added CurvedActivity derived class header

---
+++ GradedActivity/CurvedActivity.h
@@ -0,0 +1,36 @@
+#pragma once
+#include "GradedActivity.h"
+
+class CurvedActivity : public GradedActivity
+{
+protected:
+	double rawScore;
+	double percentage;
+public:
+	CurvedActivity() : GradedActivity()
+	{
+		rawScore = 0.0;
+		percentage = 0.0;
+	}
+
+	void setScore(double s)
+	{
+		rawScore = s;
+		GradedActivity::setScore(rawScore * percentage);
+	}
+
+	void setPercentage(double c)
+	{
+		percentage = c;
+	}
+
+	double getPercentage() const
+	{
+		return percentage;
+	}
+
+	double getRawScore() const
+	{
+		return rawScore;
+	}
+};
